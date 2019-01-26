e8a147b7a04e2f44bb42911d1f5d0088c2fd7e31
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-26 01:23:41 -0500

    Agregar validaciones

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,7 @@
 #
 
 class Room < ActiveRecord::Base
+validates :title, :description, :beds, :guests, :image_url, presence: true
+validates :description, length: { maximum: 400 }
+validates :beds, :guests, numericality: { only_integer: true }
 end
