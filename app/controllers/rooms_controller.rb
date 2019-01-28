4f15efa61464c16716fd0088bdf2a2cc1df74e19
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-28 16:08:02 -0500

    Agregue metodo update

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -25,7 +25,14 @@ class RoomsController < ApplicationController
     room.destroy
     redirect_to rooms_path
   end
-
+  def update
+      @room = Room.find(params[:id])
+      if @room.update(room_params)
+        redirect_to rooms_path
+      else
+        render :edit
+      end
+  end
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url, :price_per_night)
