6277fcb7f2340dd99f3de3556e900617ede26769
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-28 15:48:36 -0500

    Crear metodo destroy y edit

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,6 +15,14 @@ class RoomsController < ApplicationController
       render :new
     end
   end
+  def edit
+    @room = Room.find(room_params[:id])
+  end
+  def destroy
+    @room = Room.find(room_params[:id])
+    room.destroy
+    redirect_to rooms_path
+  end
 
   protected
     def room_params
